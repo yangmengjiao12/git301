a9029f11e5c32cc2dca51279953d796b38adf5ee
user11 <itcast@0306.com>
2018-03-28 09:57:39 +0800

    第一次提交java代码

---
+++ Demo/src/cn/itcast/demo01/DemoTest.java
@@ -0,0 +1,10 @@
+package cn.itcast.demo01;
+
+public class DemoTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+            System.out.println("第一次使用Git提交java代码");
+	}
+
+}
